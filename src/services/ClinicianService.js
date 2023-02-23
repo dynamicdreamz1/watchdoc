63ba2348d631b284e21343efbf5c100dd8b3a9b5
DD-011 <programmer116.dynamicdreamz@gmail.com>
2023-02-23 18:21:57 +0530

    search clinician service done

--- src/services/ClinicianService.js
+++ src/services/ClinicianService.js
@@ -1,13 +1,48 @@
 import axios from "axios"
+let token = sessionStorage.getItem('token')
 
-export const searchClinician = async () => {
+export const searchClinician = async (data) => {
+
+
     try {
         const response = await axios({
             method: 'get',
-            url: `${process.env.REACT_APP_ENDPOINT}user/clinic`,
+            url: `${process.env.REACT_APP_ENDPOINT}user/clinician?clinician_name=${data.clinician_name}&practice_name=${data.practice_name}&zip=${data.zip}&limit=5`,
+
+            headers: {
+                
+                "Accept": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+
         })
+        
         return response;
-    } catch (error) {
+    } 
+    catch (error) {
         return error;
     }
+}
+
+export const addDoctor=async(ID)=>{
+    // console.log(typeof ID.toString())
+    try {
+        const response = await axios({
+            method: 'post',
+            data:ID.toString(),
+            url:`${process.env.REACT_APP_ENDPOINT}user/add_clinician`,
+            headers: {
+                
+                "Accept": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+
+        })
+        return response;
+    }
+
+    catch(error){
+        return error;
+    }
+
 }
