5bbbd5778c5b0c57f0c7e29120b8a252f523a64f
DD-011 <programmer116.dynamicdreamz@gmail.com>
2023-02-28 18:14:44 +0530

    changes done

--- src/pages/AddClinicianInner.js
+++ src/pages/AddClinicianInner.js
@@ -5,7 +5,8 @@ import AddClinician from '../components/Patient/AddClinician/AddClinician';
 import Header from '../components/Templates/Header';
 
 import CliniciansRequestsTable from '../components/Clinician/CliniciansRequestsTable';
-
+import { TableSkeleton } from '../Utility/Skeleton';
+import MyClinicians from '../components/Patient/AddClinician/MyClinicians'
 export default function AddClinicianInner() {
   
   const [status,setStatus]=useState(false)
