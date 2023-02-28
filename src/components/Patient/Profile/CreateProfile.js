289a16c932c8d5993bb77aef0c6f91cb6276f87b
Jevin Designer <info@dynamicdreamz.com>
2023-02-28 10:52:25 +0530

    Create Profile and Edit Profile screen

--- src/components/Patient/Profile/CreateProfile.js
+++ src/components/Patient/Profile/CreateProfile.js
@@ -1,22 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { ProfileCreation } from '../services/UserService';
-import '../css/CreateProfile.css'
+import { ProfileCreation } from '../../../services/UserService';
+import '../../../css/CreateProfile.css'
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-
 
 const CreateProfile = () => {
     const [firstName, SetFirstName] = useState('')
     const [preferredFirstName, setPreferredFirstName] = useState('')
     const [lastName, SetLastName] = useState('')
     const [dob, SetDOB] = useState('')
- 
+    
     const [errorN, setErrorN] = useState('')
     const [success, setSuccess] = useState(false)
     const [loading, setLoading] = useState(false)
     const { t } = useTranslation()
     const navigate = useNavigate();
-
+    
     const handleSubmit = (e) => {
         e.preventDefault()
 
@@ -48,7 +47,7 @@ const CreateProfile = () => {
                     let Fname=(res.data.user_details.first_name)
                     sessionStorage.setItem('name',Fname)
                     let profileCheck = (res.data.user_details.profile_created)
-                  
+                    
                     setSuccess(true)
                     sessionStorage.setItem('profile', profileCheck)
                     navigate('/contactdetails')
@@ -77,8 +76,7 @@ const CreateProfile = () => {
                 })
         }
     }
-
-
+    
     useEffect(() => {
         if (success === true) {
             SetFirstName("")
@@ -91,48 +89,37 @@ const CreateProfile = () => {
     },
         [success]
     )
-
-
     return (
-        <React.Fragment>
-            <div className='varification-page-wrapper'>
-                <div className='container'>
-                    <div className='page-header'>
-                        <img src='/images/WatchDoc-LOGO.png' alt='Watch Doc Logo' />
-                    </div>
-                    <div className='page-content-wrapper'>
-                        <div className="title-block">
-                            <h4>{t('CreateProfilePage.register.r1')}</h4>
-                        </div>
-                        <div className='errorMessage'>{errorN}</div>
-                      
-                        <form id='main_form'>
-                            <div className='input-block'>
-                                <label htmlFor="exampleInputFirstName" >{t('CreateProfilePage.form.f1')}</label>
-                                <input type="text" placeholder={t('CreateProfilePage.form.f13')} value={firstName} id="exampleInputFirstName" onChange={(e) => SetFirstName(e.target.value)} />
-                            </div>
-
-                            <div className='input-block'>
-                                <label htmlFor="exampleInputPrefferedFirstName" >{t('CreateProfilePage.form.f17')}</label>
-                                <input type="text" placeholder={t('CreateProfilePage.form.f18')} value={preferredFirstName} id="exampleInputPrefferedFirstName" onChange={(e) => setPreferredFirstName(e.target.value)} />
-                            </div>
-
-                            <div className='input-block'>
-                                <label htmlFor="exampleInputLastName" >{t('CreateProfilePage.form.f2')}</label>
-                                <input type="text" placeholder={t('CreateProfilePage.form.f14')} value={lastName} id="exampleInputLastName" onChange={(e) => SetLastName(e.target.value)} />
-                            </div>
-                            <div className='input-block'>
-                                <label htmlFor="exampleInputDOB" >{t('CreateProfilePage.form.f3')}</label>
-                                <input type="date" value={dob} id="exampleInputDOB" onChange={(e) => SetDOB(e.target.value)} />
-                            </div>
-                           
-                            <button type="submit" onClick={(e) => handleSubmit(e)}>{t('CreateProfilePage.form.f9')}</button>
-                            {loading ? <b>{t('CreateProfilePage.loader.l1')}</b> : ""}
-                        </form>
-                    </div>
-                </div>
+        <>
+            <div className="title-block">
+                <h4>{t('CreateProfilePage.register.r1')}</h4>
             </div>
-        </React.Fragment>
+            <div className='errorMessage'>{errorN}</div>
+                      
+            <form id='main_form'>
+                <div className='input-block'>
+                    <label htmlFor="exampleInputFirstName" >{t('CreateProfilePage.form.f1')}</label>
+                    <input type="text" placeholder={t('CreateProfilePage.form.f13')} value={firstName} id="exampleInputFirstName" onChange={(e) => SetFirstName(e.target.value)} />
+                </div>
+
+                <div className='input-block'>
+                    <label htmlFor="exampleInputPrefferedFirstName" >{t('CreateProfilePage.form.f17')}</label>
+                    <input type="text" placeholder={t('CreateProfilePage.form.f18')} value={preferredFirstName} id="exampleInputPrefferedFirstName" onChange={(e) => setPreferredFirstName(e.target.value)} />
+                </div>
+
+                <div className='input-block'>
+                    <label htmlFor="exampleInputLastName" >{t('CreateProfilePage.form.f2')}</label>
+                    <input type="text" placeholder={t('CreateProfilePage.form.f14')} value={lastName} id="exampleInputLastName" onChange={(e) => SetLastName(e.target.value)} />
+                </div>
+                <div className='input-block'>
+                    <label htmlFor="exampleInputDOB" >{t('CreateProfilePage.form.f3')}</label>
+                    <input type="date" value={dob} id="exampleInputDOB" onChange={(e) => SetDOB(e.target.value)} />
+                </div>
+                
+                <button type="submit" onClick={(e) => handleSubmit(e)}>{t('CreateProfilePage.form.f9')}</button>
+                {loading ? <b>{t('CreateProfilePage.loader.l1')}</b> : ""}
+            </form>
+        </>
     )
 }
 
