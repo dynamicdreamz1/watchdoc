cc40c24cfc03166cc425aaf5adefaefebb1d8656
DD-011 <programmer116.dynamicdreamz@gmail.com>
2023-03-15 14:43:39 +0530

    file name changes done

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -4,20 +4,20 @@ import { RegisterUser } from '../services/UserService';
 import '../css/Register.css'
 import { useTranslation } from 'react-i18next';
 import { Base64 } from 'js-base64';
-import { UserContext } from '../Store/Context'; 
+import { UserContext } from '../Store/Context';
 
-const Register = () => {
+const SignIn = () => {
 
     const user = useContext(UserContext);
 
-    
+
     let navigate = useNavigate()
     const [email, setEmail] = useState('')
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const { t } = useTranslation()
 
-    
+
     const handleSubmit = (e) => {
         e.preventDefault()
 
@@ -25,36 +25,36 @@ const Register = () => {
             setError(t('RegisterPage.error.e1'))
             return
         }
-            const data = {
-                email: email
-            }
-            setLoading(true)
+        const data = {
+            email: email
+        }
+        setLoading(true)
 
-            RegisterUser(data)
-                .then((response) => {
-                    if (typeof response === "string") {
-                        setError(response)
-                        setLoading(false)
-                        
-                    } else {
-                        let encodedemail = Base64.encode(response?.data?.data?.email)
-                        
-                        setLoading(false)
-                        setEmail("")
-                        navigate(`/verification/${encodedemail}`)
-                        console.log(response)
-                    }
-                })
-                .catch((error) => {
-                    console.log(error);
-                    return error
-                })
-       
+        RegisterUser(data)
+            .then((response) => {
+                if (typeof response === "string") {
+                    setError(response)
+                    setLoading(false)
+
+                } else {
+                    let encodedemail = Base64.encode(response?.data?.data?.email)
+
+                    setLoading(false)
+                    setEmail("")
+                    navigate(`/verification/${encodedemail}`)
+                    console.log(response)
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                return error
+            })
+
     }
 
     if (user?.token) {
         return redirect('/dashboard');
-       }
+    }
 
 
     return (
@@ -72,8 +72,8 @@ const Register = () => {
 
                         <form>
                             <div className='default-login'>
-                                <input type="email" placeholder={t('RegisterPage.form.f1')} 
-                                value={email} id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} />
+                                <input type="email" placeholder={t('RegisterPage.form.f1')}
+                                    value={email} id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} />
                                 <button type="submit" onClick={(e) => handleSubmit(e)}>{t('RegisterPage.form.f2')}</button>
                             </div>
                             {loading ? <b>{t('RegisterPage.loader.l1')}</b> : ""}
@@ -94,4 +94,4 @@ const Register = () => {
     )
 }
 
-export default Register;
+export default SignIn;
