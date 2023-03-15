dbed1fd88acf4cbb37d855b05befc99e18b2c749
Jevin Designer <info@dynamicdreamz.com>
2023-03-15 15:21:54 +0530

    Clinician Signup Screen

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -5,19 +5,21 @@ import '../css/Register.css'
 import { useTranslation } from 'react-i18next';
 import { Base64 } from 'js-base64';
 import { UserContext } from '../Store/Context'; 
+import { IconButton, InputAdornment } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
-const Register = () => {
+const SignIn = () => {
 
     const user = useContext(UserContext);
 
-    
+
     let navigate = useNavigate()
     const [email, setEmail] = useState('')
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const { t } = useTranslation()
 
-    
+
     const handleSubmit = (e) => {
         e.preventDefault()
 
@@ -25,37 +27,53 @@ const Register = () => {
             setError(t('RegisterPage.error.e1'))
             return
         }
-            const data = {
-                email: email
-            }
-            setLoading(true)
+        const data = {
+            email: email
+        }
+        setLoading(true)
 
-            RegisterUser(data)
-                .then((response) => {
-                    if (typeof response === "string") {
-                        setError(response)
-                        setLoading(false)
-                        
-                    } else {
-                        let encodedemail = Base64.encode(response?.data?.data?.email)
-                        
-                        setLoading(false)
-                        setEmail("")
-                        navigate(`/verification/${encodedemail}`)
-                        console.log(response)
-                    }
-                })
-                .catch((error) => {
-                    console.log(error);
-                    return error
-                })
-       
+        RegisterUser(data)
+            .then((response) => {
+                if (typeof response === "string") {
+                    setError(response)
+                    setLoading(false)
+
+                } else {
+                    let encodedemail = Base64.encode(response?.data?.data?.email)
+
+                    setLoading(false)
+                    setEmail("")
+                    navigate(`/verification/${encodedemail}`)
+                    console.log(response)
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                return error
+            })
+
     }
+
+    const [values, setValues] = React.useState({
+        password: "",
+        showPassword: false,
+    });
+    
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handlePasswordChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
 
     if (user?.token) {
         return redirect('/dashboard');
-       }
-
+    }
 
     return (
         <React.Fragment>
@@ -68,22 +86,44 @@ const Register = () => {
                         </div>
                     </div>
                     <div className='form-block'>
+                        <div className='form-title text-center'>
+                            <h1>Sign in</h1>
+                        </div>
                         <div className='LoginError'>{error && error}</div>
 
                         <form>
-                            <div className='default-login'>
+                            <div className='input-block'>
                                 <input type="email" placeholder={t('RegisterPage.form.f1')} 
                                 value={email} id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+                            <div className='input-block'>
+                                <input type={values.showPassword ? "text" : "password"}
+                                onChange={handlePasswordChange("password")}
+                                value={values.password}
+                                endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    >
+                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                                } />
+                            </div>
+                            <div className='forgot-pw-block'>
+                                <a href={{}}>Forgot Password</a>
+                            </div>
+                            <div className='submit-block'>
                                 <button type="submit" onClick={(e) => handleSubmit(e)}>{t('RegisterPage.form.f2')}</button>
                             </div>
                             {loading ? <b>{t('RegisterPage.loader.l1')}</b> : ""}
-                            <div className='or-text'>
-                                <span>{t('RegisterPage.form.f3')}</span>
+                            <div className='not-user-block text-center'>
+                                <span>Not with us?</span>
+                                <a href={{}}>Sign up now</a>
                             </div>
-                            <div className='login-options'>
-                                <button type='button' ><img src="/images/google-icon.png" alt="Google" />{t('RegisterPage.form.f4')}</button>
-                                <button type='button' className="apple-icon"><img src="/images/apple-icon.png" alt="Apple" />{t('RegisterPage.form.f5')}</button>
-                                <button type='button'><img src="/images/key-icon.png" alt="SSO Icon" />{t('RegisterPage.form.f6')}</button>
+                            <div className='privacy-policy-text text-center'>
+                                <a href={{}}>View Privacy Policy</a>
                             </div>
                         </form>
                     </div>
@@ -94,4 +134,4 @@ const Register = () => {
     )
 }
 
-export default Register;
+export default SignIn;
