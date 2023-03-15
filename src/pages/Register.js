b502e67a58d5f57b4a71c43a4007e8f40e1bba1f
Jevin Designer <info@dynamicdreamz.com>
2023-03-15 11:35:05 +0530

    Sign In screen Design

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -60,37 +60,32 @@ const Register = () => {
     return (
         <React.Fragment>
 
-            <div className='login-page-wrapper page-wrapper'>
-                <div className='container'>
-                    <div className='wrapper'>
-                        <div className='logo-block'>
-                            <div className='logo'>
-                                <img src='/images/WatchDoc-LOGO.png' alt="WatchDoc Logo" />
-                            </div>
-                            <div className='frame'>
-                                <img src='/images/Mobile-Frame.png' alt="Mobile Frame with User Hand" />
-                            </div>
+            <div className='page-wrapper'>
+                <div className='signin-box'>
+                    <div className='logo-block'>
+                        <div className='logo'>
+                            <img src='/images/WatchDoc-LOGO.svg' alt="WatchDoc Logo" />
                         </div>
-                        <div className='form-block'>
-                            <div className='LoginError'>{error && error}</div>
+                    </div>
+                    <div className='form-block'>
+                        <div className='LoginError'>{error && error}</div>
 
-                            <form>
-                                <div className='default-login'>
-                                    <input type="email" placeholder={t('RegisterPage.form.f1')} 
-                                    value={email} id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} />
-                                    <button type="submit" onClick={(e) => handleSubmit(e)}>{t('RegisterPage.form.f2')}</button>
-                                </div>
-                                {loading ? <b>{t('RegisterPage.loader.l1')}</b> : ""}
-                                <div className='or-text'>
-                                    <span>{t('RegisterPage.form.f3')}</span>
-                                </div>
-                                <div className='login-options'>
-                                    <button type='button' ><img src="/images/google-icon.png" alt="Google" />{t('RegisterPage.form.f4')}</button>
-                                    <button type='button' className="apple-icon"><img src="/images/apple-icon.png" alt="Apple" />{t('RegisterPage.form.f5')}</button>
-                                    <button type='button'><img src="/images/key-icon.png" alt="SSO Icon" />{t('RegisterPage.form.f6')}</button>
-                                </div>
-                            </form>
-                        </div>
+                        <form>
+                            <div className='default-login'>
+                                <input type="email" placeholder={t('RegisterPage.form.f1')} 
+                                value={email} id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} />
+                                <button type="submit" onClick={(e) => handleSubmit(e)}>{t('RegisterPage.form.f2')}</button>
+                            </div>
+                            {loading ? <b>{t('RegisterPage.loader.l1')}</b> : ""}
+                            <div className='or-text'>
+                                <span>{t('RegisterPage.form.f3')}</span>
+                            </div>
+                            <div className='login-options'>
+                                <button type='button' ><img src="/images/google-icon.png" alt="Google" />{t('RegisterPage.form.f4')}</button>
+                                <button type='button' className="apple-icon"><img src="/images/apple-icon.png" alt="Apple" />{t('RegisterPage.form.f5')}</button>
+                                <button type='button'><img src="/images/key-icon.png" alt="SSO Icon" />{t('RegisterPage.form.f6')}</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
