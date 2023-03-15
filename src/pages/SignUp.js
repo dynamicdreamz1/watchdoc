37e0fc923e84c7123bd02eb6de2797712716bd74
Jevin Designer <info@dynamicdreamz.com>
2023-03-15 14:53:54 +0530

    Clinician Signup Screen

---
+++ src/pages/SignUp.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const SignUp = () =>  {
+  return (
+    <>
+        <div className='page-wrapper'>
+            <div className='form-block'>
+                <div className='logo-block'>
+                    <img src='/images/WatchDoc-LOGO.svg' alt='WatchDoc Logo' />
+                </div>
+                <div className='text-block'>
+                    <h1>Create your free clinician listing</h1>
+                    <p>Create a free profile on WatchDoc and make it easy for your patients to find and connect with you via the WatchDoc app.</p>
+                </div>
+                <form>
+                    <div className='input-block'>
+                        <input type="text" name='first-name' placeholder='First Name' required />
+                    </div>
+                    <div className='input-block'>
+                        <input type="text" name='last-name' placeholder='Last Name' required />
+                    </div>
+                    <div className='input-block'>
+                        <input type="email" name='email' placeholder='Email' required />
+                    </div>
+                    <div className='input-block'>
+                        <input type="text" name='mobile' placeholder='Mobile' required />
+                    </div>
+                    <div className='input-block'>
+                        <input type="text" name='practice-name' placeholder='Practice name' required />
+                    </div>
+                    <div className='input-block'>
+                        <input type="text" name='practice-address' placeholder='Practice Address' required />
+                    </div>
+                    <div className='submit-block'>
+                        <button type='submit'>Sign Up</button>
+                    </div>
+                </form>
+            </div>
+            <div className='image-block'>
+                <img src='/images/Lady_with_laptop_register.png' alt='Lady with Laptop' />
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default SignUp;
