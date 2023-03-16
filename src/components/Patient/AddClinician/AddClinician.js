bc2a6ba25f6f2c46cc39f5d5a3cd40319bf5f61d
Jevin Designer <info@dynamicdreamz.com>
2023-03-16 17:55:44 +0530

    Css changes

--- src/components/Patient/AddClinician/AddClinician.js
+++ src/components/Patient/AddClinician/AddClinician.js
@@ -69,7 +69,7 @@ export default function AddClinician({ status, setStatus }) {
               </div>
 
               <PractitionersCard  status={status} setStatus={setStatus} isSkeleton={isSkeleton}/>
-              { nextBtn ?  <button onClick={()=>navigate("/dashboard")}>Next</button> : ""  } 
+              { nextBtn ?  <div className='btn-block'><button className='btn' onClick={()=>navigate("/dashboard")}>Next</button></div> : ""  } 
             </>
             : ""
 
