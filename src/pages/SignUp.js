0a6be8f6009c89de499a84ebde6d872ea5263770
Jevin Designer <info@dynamicdreamz.com>
2023-03-17 15:24:05 +0530

    Sign up screen design

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,14 +3,14 @@ import React from 'react'
 const SignUp = () =>  {
   return (
     <>
-        <div className='page-wrapper'>
+        <div className='page-wrapper signup-page-wrapper'>
             <div className='form-block'>
                 <div className='logo-block'>
                     <img src='/images/WatchDoc-LOGO.svg' alt='WatchDoc Logo' />
                 </div>
                 <div className='text-block'>
                     <h1>Create your free clinician listing</h1>
-                    <p>Create a free profile on WatchDoc and make it easy for your patients to find and connect with you via the WatchDoc app.</p>
+                    <p>Create a free profile on WatchDoc and make it easy for your<br></br> patients to find and connect with you via the WatchDoc app.</p>
                 </div>
                 <form>
                     <div className='input-block'>
