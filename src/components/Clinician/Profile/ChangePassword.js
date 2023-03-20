73a61522a9acea16810031bc1e675b8ea380fe34
Jevin Designer <info@dynamicdreamz.com>
2023-03-20 18:38:15 +0530

    My Profiles screen

--- src/components/Clinician/Profile/ChangePassword.js
+++ src/components/Clinician/Profile/ChangePassword.js
@@ -10,7 +10,19 @@ export default function ChangePassword() {
             </div>
             <form>
                 <div className='input-block'>
-                    <input type="password" name='current-password'></input>
+                    <label>Current password</label>
+                    <input type="password" name='current-password' placeholder='Enter current password'></input>
+                </div>
+                <div className='input-block'>
+                    <label>New password</label>
+                    <input type="password" name='new-password' placeholder='Enter a new password'></input>
+                </div>
+                <div className='input-block'>
+                    <label>Confirm new password</label>
+                    <input type="password" name='confirm-password' placeholder='Confirm your new password'></input>
+                </div>
+                <div className='submit-block'>
+                    <button type="submit">Save</button>
                 </div>
             </form>
         </div>
--- src/components/Clinician/Profile/MyProfile.js
+++ src/components/Clinician/Profile/MyProfile.js
@@ -1,4 +1,6 @@
+import { MenuItem, Select } from '@mui/material'
 import React from 'react'
+import { SelectChangeEvent } from '@mui/material/Select';
 
 export default function MyProfile() {
   return (
@@ -7,6 +9,48 @@ export default function MyProfile() {
             <div className='title-block'>
                 <h2>Profile</h2>
             </div>
+            <form>
+            <div className='input-block'>
+                <div className='inputs-wrapper'>
+                    <div className='input-item'>
+                        <label>Title</label>
+                        {/* <Select
+                            labelId="demo-simple-select-label"
+                            value={age}
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={}>Ten</MenuItem>
+                            <MenuItem value={}>Twenty</MenuItem>
+                            <MenuItem value={}>Thirty</MenuItem>
+                        </Select> */}
+                        <input type="password" name='email-address'></input>
+                    </div>
+                    <div className='input-item'>
+                        <label>First name</label>
+                        <input type="text" name='first-name'></input>
+                    </div>
+                    <div className='input-item'>
+                        <label>Last name</label>
+                        <input type="text" name='last-name'></input>
+                    </div>
+                </div>
+            </div>
+                <div className='input-block'>
+                    <label>Email address</label>
+                    <input type="password" name='email-address'></input>
+                </div>
+                <div className='input-block'>
+                    <label>Practice name</label>
+                    <input type="password" name='practice-name'></input>
+                </div>
+                <div className='input-block'>
+                    <label>Practice Address</label>
+                    <input type="password" name='practice-address'></input>
+                </div>
+                <div className='submit-block'>
+                    <button type="submit">Save</button>
+                </div>
+            </form>
         </div>
     </>
   )
