1212fea00eacbb413eaf558efb2d0ad7e6ac0e4b
dynamic113dreamz <117342675+dynamic113dreamz@users.noreply.github.com>
2023-03-21 10:21:51 +0530

    formik yup added in signup

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { Formik } from 'formik';
+import * as Yup from "yup";
 
 const SignUp = () => {
     const navigate = useNavigate();
-    // const [error,setError]=useState("")
 
     const [signUpUserData, setSignUpUserData] = useState({
         "firstname": "",
@@ -13,95 +14,41 @@ const SignUp = () => {
         "practicename": "",
         "practiceaddress": ""
     })
-    const [firstnameError, setFirstnameError] = useState("")
-    const [lastnameError, setLastnameError] = useState("")
-    const [emailError, setEmailError] = useState("")
-    const [mobileError, setMobileError] = useState("")
-    const [practicenameError, setPracticenameError] = useState("")
-    const [practiceaddressError, setPracticeaddressError] = useState("")
+    
 
+    const LoginSchema = Yup.object({
+        firstname: Yup.string().required("This field is required*")
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+        lastname: Yup.string().required("This field is required*")
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+        email: Yup.string().required("Email Is Required")
+        .matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please Enter Valid Email"),
+        mobile: Yup.string().required("This field is required*")
+        .matches(/^[0-9\b]+$/, "Please Enter valid Number"),
+        practicename: Yup.string().required("This field is required*")
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+        practiceaddress: Yup.string().required("This field is required*")
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+      });
 
+ 
 
-    const isValidateEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(signUpUserData?.email);
-    const isvalidateNumber = /^[0-9\b]+$/;
-    const  regexText = /^[a-zA-Z]*$/;
-
-    const handleChange = (key, value) => {
-        const temp = ({ ...signUpUserData, [key]: value })
-        let withoutSpace = temp?.mobile.replace(/ /g,"");
-let length = withoutSpace.length;
-        isvalidateNumber.test(temp?.mobile)&& length===10?setMobileError(""):setMobileError("Please enter Valid Number Format*")
-        temp?.firstname === "" || !regexText.test(temp?.firstname) ? setFirstnameError("This field is required*") : setFirstnameError("");
-        temp?.lastname === "" ? setLastnameError("This field is required*") : setLastnameError("");
-        !isValidateEmail ? setEmailError("Please enter valid email format*") : setEmailError("");
-        !isvalidateNumber.test(temp?.mobile) ? setMobileError("Please enter Valid Number Format*") : setEmailError("");
-        temp?.practicename === "" ? setPracticenameError("This field is required*") : setPracticenameError("");
-        temp?.practiceaddress === "" ? setPracticeaddressError("This field is required*") : setPracticeaddressError("");
-        temp?.firstname !== "" && regexText.test(temp?.firstname)?setFirstnameError(""):setFirstnameError("This field is required*")
-        temp?.lastname !== "" && regexText.test(temp?.lastname)?setLastnameError(""):setLastnameError("This field is required*")
-        temp?.practicename !== "" && regexText.test(temp?.practicename)?setPracticenameError(""):setPracticenameError("This field is required*")
-        temp?.practiceaddress !== "" && regexText.test(temp?.practiceaddress)?setPracticeaddressError(""):setPracticeaddressError("This field is required*")
-
-      
-        if (temp?.firstname !== "" &&
-        regexText.test(temp?.firstname) &&
-        regexText.test(temp?.lastname) 
-        & regexText.test(temp?.practicename)
-        && regexText.test(temp?.practiceaddress)&&
-         temp?.lastname !== "" && isValidateEmail && isvalidateNumber.test(temp?.mobile)
-            && temp?.practicename !== "" && temp?.practiceaddress !== "") {
-            setFirstnameError("")
-            setLastnameError("")
-            setEmailError("")
-            setMobileError("")
-            setMobileError("")
-            setPracticenameError("")
-            setPracticeaddressError("")
-
-
-        }
-        setSignUpUserData({ ...signUpUserData, [key]: value })
-
-    }
-    const handleSubmitForm = (e) => {
-        e.preventDefault();
-        let withoutSpace = signUpUserData?.mobile.replace(/ /g,"");
-let length = withoutSpace.length;
-        isvalidateNumber.test(signUpUserData?.mobile)&& length===10?setMobileError(""):setMobileError("Please enter Valid Number Format*")
-        signUpUserData?.firstname === ""? setFirstnameError("This field is required*") : setFirstnameError("");
-        signUpUserData?.lastname === "" ? setLastnameError("This field is required*") : setLastnameError("");
-        !isValidateEmail ? setEmailError("Please enter valid email format*") : setEmailError("");
-        !isvalidateNumber.test(signUpUserData?.mobile) ? setMobileError("Please enter Valid Number Format*") : setEmailError("");
-        signUpUserData?.practicename === "" ? setPracticenameError("This field is required*") : setPracticenameError("");
-        signUpUserData?.practiceaddress === "" ? setPracticeaddressError("This field is required*") : setPracticeaddressError("");
-        signUpUserData?.firstname !== "" && regexText.test(signUpUserData?.firstname)?setFirstnameError(""):setFirstnameError("This field is required*")
-        signUpUserData?.lastname !== "" && regexText.test(signUpUserData?.lastname)?setLastnameError(""):setLastnameError("This field is required*")
-        signUpUserData?.practicename !== "" && regexText.test(signUpUserData?.practicename)?setPracticenameError(""):setPracticenameError("This field is required*")
-        signUpUserData?.practiceaddress !== "" && regexText.test(signUpUserData?.practiceaddress)?setPracticeaddressError(""):setPracticeaddressError("This field is required*")
-        if (signUpUserData?.firstname !== "" && 
-        (isvalidateNumber.test(signUpUserData?.mobile) && length===10)&&
-        regexText.test(signUpUserData?.firstname) &&
-        regexText.test(signUpUserData?.lastname) 
-        & regexText.test(signUpUserData?.practicename)
-        && regexText.test(signUpUserData?.practiceaddress)
-        && signUpUserData?.lastname !== "" && isValidateEmail && isvalidateNumber.test(signUpUserData?.mobile)
-            && signUpUserData?.practicename !== "" && signUpUserData?.practiceaddress !== "") {
-            setFirstnameError("")
-            setLastnameError("")
-            setEmailError("")
-            setMobileError("")
-            setMobileError("")
-            setPracticenameError("")
-            setPracticeaddressError("")
-            navigate('/signupsuccess')
-
-        }
-
-
+    
+    const handleSubmitForm = (data) => {
+        setSignUpUserData({...data})
+        navigate("/signupsuccess")
     }
 
 
     return (
+        <Formik 
+        initialValues={signUpUserData}
+        enableReinitialize={true}
+        validationSchema={LoginSchema}
+        onSubmit={(values) =>
+        { handleSubmitForm(values)}} 
+> 
+{(props) => (
         <>
 
             <div className='page-wrapper signup-page-wrapper'>
@@ -113,53 +60,57 @@ let length = withoutSpace.length;
                         <h1>Create your free clinician listing</h1>
                         <p>Create a free profile on WatchDoc and make it easy for your<br></br> patients to find and connect with you via the WatchDoc app.</p>
                     </div>
-                    <form>
+                    <form onSubmit={props.handleSubmit}>
                         <div className='input-block'>
-                            <input type="text" name='first-name' placeholder='First Name*' onChange={(e) => handleChange("firstname", e.target.value)} />
-                            <span className="error">{firstnameError !== "" ? firstnameError : ""}</span>
+                            <input type="text" name='firstname' placeholder='First Name*'  onChange={props.handleChange} value={props.values.firstname}/>
+                            <span className="error">{props.errors.firstname?props.errors.firstname:""}</span>
                         </div>
                         
                         <div className='input-block'>
-                            <input type="text" name='last-name' placeholder='Last Name*' onChange={(e) => handleChange("lastname", e.target.value)} />
-                            <span className="error"> {lastnameError !== "" ? lastnameError : ""}</span>
+                            <input type="text" name='lastname' placeholder='Last Name*'  onChange={props.handleChange} value={props.values.lastname} />
+                            <span className="error"> {props.errors.lastname?props.errors.lastname:""}</span>
 
                         </div>
                        
 
                         <div className='input-block'>
-                            <input type="email" name='email' placeholder='Email*' onChange={(e) => handleChange("email", e.target.value)} />
-                            <span className="error">  {emailError !== "" ? emailError : ""}</span>
+                            <input type="email" name='email' placeholder='Email*'  onChange={props.handleChange} value={props.values.email}/>
+                            <span className="error">  {props.errors.email?props.errors.email:""}</span>
                         </div>
                        
 
 
                         <div className='input-block'>
-                            <input type="text" name='mobile' placeholder='Mobile*' onChange={(e) => handleChange("mobile", e.target.value)}  maxLength="10" />
-                            <span className="error"> {mobileError !== "" ? mobileError : ""}</span>
+                            <input type="text" name='mobile' placeholder='Mobile*'  onChange={props.handleChange}  maxLength="10" value={props.values.mobile}/>
+                            <span className="error"> {props.errors.mobile?props.errors.mobile:""}</span>
                         </div>
                         
 
                         <div className='input-block'>
-                            <input type="text" name='practice-name' placeholder='Practice name*' onChange={(e) => handleChange("practicename", e.target.value)} />
-                            <span className="error">{practicenameError !== "" ? practicenameError : ""}</span>
+                            <input type="text" name='practicename' placeholder='Practice name*'  onChange={props.handleChange} value={props.values.practicename}/>
+                            <span className="error">{props.errors.practicename?props.errors.practicename:""}</span>
                         </div>
                         
 
 
 
                         <div className='input-block'>
-                            <input type="text" name='practice-address' placeholder='Practice Address*' onChange={(e) => handleChange("practiceaddress", e.target.value)} />
-                            <span className="error">{practiceaddressError !== "" ? practiceaddressError : ""}</span>
+                            <input type="text" name='practiceaddress' placeholder='Practice Address*'  onChange={props.handleChange} value={props.values.practiceaddress} />
+                            <span className="error">{props.errors.practiceaddress?props.errors.practiceaddress:""}</span>
                         </div>
                         
 
                         <div className='submit-block'>
-                            <button type='submit' onClick={handleSubmitForm}>Sign Up</button>
+                            <button type='submit' >Sign Up</button>
                         </div>
                     </form>
+                    
                 </div>
             </div>
         </>
+
+ )}
+      </Formik>
     )
 }
 
