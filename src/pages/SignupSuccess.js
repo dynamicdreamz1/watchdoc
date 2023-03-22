b0e63c902e31872d04eb9c8424dad6da8f2556b3
DD-011 <programmer116.dynamicdreamz@gmail.com>
2023-03-22 14:21:35 +0530

    translation done in signupsuccess page

--- src/pages/SignupSuccess.js
+++ src/pages/SignupSuccess.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 export default function SignupSuccess() {
+  const {t}=useTranslation()
   return (
     <>
         <div className='page-wrapper success-page-wrapper'>
             <div className='text-block'>
                 <div className='text'>
-                    <h1>Success! Thanks for registering.</h1>
-                    <p>Thanks for signing up for a WatchDoc profile. Our support team will be in touch shortly to complete your registration and run you through the WatchDoc system.</p>
-                    <button type='button' className='btn'>Back to main site</button>
+                    <h1> {t('SignupSuccessPage.heading')} </h1>
+                    <p> {t('SignupSuccessPage.para1')} </p>
+                    <button type='button' className='btn'> {t('SignupSuccessPage.button')} </button>
                 </div>
             </div>
         </div>
