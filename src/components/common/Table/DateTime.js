a6a56e9cad3ed2ef2c24085b77441ae7dfb06198
DD-011 <programmer116.dynamicdreamz@gmail.com>
2023-03-24 14:59:29 +0530

    sorting

--- src/components/common/Table/DateTime.js
+++ src/components/common/Table/DateTime.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function DateTime(props) {
   return (
     <>
-        <span className="time">{props?.props?.date}</span>
+        <span className="time">{props?.props?.date.toDateString()}</span>
     </>
   )
 }
