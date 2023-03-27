ba3b8f900e745c5fe6f4b1f2b4834c4cd5f0c8a5
dynamic113dreamz <117342675+dynamic113dreamz@users.noreply.github.com>
2023-03-27 14:43:26 +0530

    working on validation

--- src/components/Admin/AddStaffUser.js
+++ src/components/Admin/AddStaffUser.js
@@ -4,13 +4,16 @@ import "/node_modules/flag-icons/css/flag-icons.min.css";
 import { allTimeZone } from '../../Utility/countryCode';
 import { Formik } from 'formik';
 import * as Yup from "yup";
+import { useTranslation } from 'react-i18next';
 
 export default function AddStaffUser({staffUser,setOpen}) {
+    const { t } = useTranslation()
 
     const [countryCode, setcountryCode] = useState('');
     const [ imageUrl, setImgSrc ] = useState("/images/user-picture-placeholder.png");
     const [addNewStaff,setAddNewStaff]=useState({
         "id": staffUser.length+1,
+        "title":"Dr",
         "firstname":"",
         "lastname":"",
         "email": "",
@@ -26,18 +29,23 @@ export default function AddStaffUser({staffUser,setOpen}) {
     })
 
 
-    // const LoginSchema = Yup.object({
-    //     firstname: Yup.string().required("This field is required*")
-    //     .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
-    //     lastname: Yup.string().required("This field is required*")
-    //     .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
-    //     email: Yup.string().required("Email Is Required")
-    //     .matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please Enter Valid Email"),
-    //     practicename: Yup.string().required("This field is required*")
-    //     .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
-    //     practiceaddress: Yup.string().required("This field is required*")
-        
-    // });
+    const LoginSchema = Yup.object({
+        firstname: Yup.string().required("This field is required*")
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+        lastname: Yup.string().required("This field is required*")
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+        email: Yup.string().required("Email Is Required")
+        .matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please Enter Valid Email"),
+        practicename: Yup.string().required("This field is required*")
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+        practiceaddress: Yup.string().required("This field is required*"),
+        number: Yup.string().required(t('SignUpPage.validation.common1'))
+            .matches(/"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"/gmi, t('SignUpPage.validation.mobile.v1')),
+            // .min(1, t('SignUpPage.validation.mobile.short'))
+            // .max(10, t('SignUpPage.validation.mobile.long')),
+        password:Yup.string().required("This field is required*")
+        .matches("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$","Enter Valid Password")
+    });
 
 
 
@@ -76,6 +84,7 @@ export default function AddStaffUser({staffUser,setOpen}) {
 
 
     const handleSubmitForm = (data) => {
+        console.log("11111-data",data)
         const finalData={
             "id": data?.id,
             "name": data?.firstname,
@@ -87,7 +96,7 @@ export default function AddStaffUser({staffUser,setOpen}) {
                 {
                     "id": 11,
                     "meta_key": "full_name",
-                    "meta_value": `${data?.firstname} ${data?.lastname}`
+                    "meta_value": `${data?.title} ${data?.firstname} ${data?.lastname}`
                 },
                 {
                     "id": 13,
@@ -137,7 +146,7 @@ export default function AddStaffUser({staffUser,setOpen}) {
         <Formik 
         initialValues={addNewStaff}
         enableReinitialize={true}
-        validationSchema=""
+        validationSchema={LoginSchema}
         onSubmit={(values) =>
         { handleSubmitForm(values)}} 
     > 
@@ -160,7 +169,7 @@ export default function AddStaffUser({staffUser,setOpen}) {
                     <div className='inputs-wrapper'>
                         <div className='input-item'>
                             <label>Title</label>
-                            <select name='title'>
+                            <select name='title' defaultValue={props?.values?.title} onChange={props?.handleChange}>
                                 <option value="Dr">Dr</option>
                                 <option value="Hospital">Hospital</option>
                             </select>
@@ -168,33 +177,34 @@ export default function AddStaffUser({staffUser,setOpen}) {
                         <div className='input-item'>
                             <label>First name</label>
                             <input type="text" name='firstname' value={props?.values?.firstname} onChange={props?.handleChange}/>
-                            <span className="error"></span>
+                            <span className="error">{props.errors.firstname ? props.errors.firstname : ""}</span>
                         </div>
                         <div className='input-item'>
                             <label>Last name</label>
                             <input type="text" name='lastname' value={props?.values?.lastname} onChange={props?.handleChange}/>
-                            <span className="error"></span>
+                            <span className="error"> {props.errors.lastname ? props.errors.lastname : ""}</span>
                         </div>
                     </div>
                 </div>
                 <div className='input-block'>
                     <label>Email address</label>
                     <input type="email" name='email' value={props?.values?.email} onChange={props?.handleChange}/>
-                    <span className="error"></span>
+                    <span className="error">  {props.errors.email ? props.errors.email : ""}</span>
                 </div>
                 <div className='input-block'>
                     <label>Practice name</label>
                     <input type="text" name='practicename' value={props?.values?.practicename} onChange={props?.handleChange}/>
-                    <span className="error"></span>
+                    <span className="error">{props.errors.practicename?props.errors.practicename:""}</span>
                 </div>
                 <div className='input-block'>
                     <label>Practice Address</label>
                     <input type="text" name='practiceaddress' value={props?.values?.practiceaddress} onChange={props?.handleChange}/>
-                    <span className="error"></span>
+                    <span className="error">{props.errors.practiceaddress?props.errors.practiceaddress:""}</span>
                 </div>
                 <div className='input-block'>
                     <label>Password</label>
                     <input type="password" name='password' value={props?.values?.password} onChange={props?.handleChange}/>
+                    <span className="error">{props.errors.password?props.errors.password:""}</span>
                 </div>
                 <div className='input-block country-code'>
                 <label id="country-code">Enter new phone number</label>
@@ -210,6 +220,7 @@ export default function AddStaffUser({staffUser,setOpen}) {
                     ))}
                   </Select>
                   <input type="text" name="number" value={props?.values?.number} onChange={props?.handleChange}></input>
+                  <span className="error"> {props.errors.number ? props.errors.number : ""}</span>
                 </div>
               </div>
                 <div className='submit-block'>
