4f5d4824ddaeea361644a26f31b7a037429aea96
DD-011 <programmer116.dynamicdreamz@gmail.com>
2023-03-27 14:41:37 +0530

    condtional display in clinician data done

--- src/components/common/Table/ClinicianInfoRow.js
+++ src/components/common/Table/ClinicianInfoRow.js
@@ -6,7 +6,7 @@ import Email from './Email'
 import PendingPatients from './PendingPatients'
 import Phone from './Phone'
 
-export default function ClinicianInfoRow({data}) {
+export default function ClinicianInfoRow({value,data}) {
 
   return (
     <>
@@ -14,8 +14,12 @@ export default function ClinicianInfoRow({data}) {
         <TableCell><ClinicianInfo data={data}/></TableCell>
         <TableCell><Email email={data?.email}/></TableCell>
         <TableCell><Phone data={data}/></TableCell>
+        {value === 0 ? "" :
+        <>
         <TableCell align='center'><ConnectedPatients/></TableCell>
         <TableCell align='center'><PendingPatients/></TableCell>
+        </>
+      }
     </TableRow>
     </>
   )
