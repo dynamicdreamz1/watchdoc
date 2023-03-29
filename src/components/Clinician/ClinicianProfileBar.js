7240480de194994548ca9f62875dca82aa7fc7e5
Jevin Designer <info@dynamicdreamz.com>
2023-03-29 15:24:13 +0530

    Clinician Profile Dialog

--- src/components/Clinician/ClinicianProfileBar.js
+++ src/components/Clinician/ClinicianProfileBar.js
@@ -1,9 +1,19 @@
-import { Button } from '@mui/material'
+import { Button, Dialog } from '@mui/material'
 import React from 'react'
 import PhoneNumber from '../common/PhoneNumber'
 import UserProfile from '../common/UserProfile'
 
 export default function ClinicianProfileBar({profileBarData}) {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
@@ -13,7 +23,15 @@ export default function ClinicianProfileBar({profileBarData}) {
             </div>
             <div className='right-block d-flex align-items-center'>
                 <PhoneNumber number={profileBarData?.contact_number}/>
-                <Button>Profile</Button>
+                <Button onClick={handleClickOpen}>Profile</Button>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="clinician-profile-dialog"
+                  aria-describedby="clinician-profile-dialog"
+                >
+
+                </Dialog>
             </div>
         </div>
     </>
