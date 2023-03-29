9df36dc3223e6a194edf83f2fbbfd319510bf4b9
DD-011 <programmer116.dynamicdreamz@gmail.com>
2023-03-29 17:07:27 +0530

    console warning message issue resolved

--- src/components/Admin/ClinicianRequest.js
+++ src/components/Admin/ClinicianRequest.js
@@ -8,35 +8,35 @@ export default function ClinicianRequest() {
                 <div className='inputs-wrapper'>
                     <div className='input-item'>
                         <label>Title</label>
-                        <input type="text" name="title" value="Dr" />
+                        <input type="text" name="title" defaultValue="Dr" />
                     </div>
                     <div className='input-item'>
                         <label>First name</label>
-                        <input type="text" name='firstname' value="Sarah" />
+                        <input type="text" name='firstname' defaultValue="Sarah" />
                     </div>
                     <div className='input-item'>
                         <label>Last name</label>
-                        <input type="text" name='lastname' value="McDonnell"/>
+                        <input type="text" name='lastname' defaultValue="McDonnell"/>
                     </div>
                 </div>
             </div>
             <div className='input-block'>
                 <label>Email address</label>
-                <input type="email" name='email' value="info@neighbourhoodmedical.com.au" />
+                <input type="email" name='email' defaultValue="info@neighbourhoodmedical.com.au" />
             </div>
             <div className='input-block'>
                 <label>Practice name</label>
-                <input type="text" name='practicename' value="Neighbourhood Medical" />
+                <input type="text" name='practicename' defaultValue="Neighbourhood Medical" />
             </div>
             <div className='input-block'>
                 <label>Practice Address</label>
-                <input type="text" name='practiceaddress' value="1A Stuartholme Road, Bardon QLD 4065" />
+                <input type="text" name='practiceaddress' defaultValue="1A Stuartholme Road, Bardon QLD 4065" />
             </div>
             <div className='input-block country-code'>
                 <label id="country-code">Mobile number</label>
                 <div className='inputs-wrapper'>
                     <input type="text" />
-                    <input type="text" name="number" value="451514497" />
+                    <input type="text" name="number" defaultValue="451514497" />
                 </div>
             </div>
         </form>
