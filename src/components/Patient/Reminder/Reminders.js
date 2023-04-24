9d587b345e275e1e6ba1f657484b9d4cc110d188
dynamic113dreamz <117342675+dynamic113dreamz@users.noreply.github.com>
2023-04-24 16:41:23 +0530

    reminder popup

--- src/components/Patient/Reminder/Reminders.js
+++ src/components/Patient/Reminder/Reminders.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useTranslation } from 'react-i18next'
 import ReminderCard from './ReminderCard'
-import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import WeightYourselfReminderOverlay from '../../Clinician/Overlays/WeightYourselfReminderOverlay';
 
 export default function Reminders() {
+  const [open,setOpen]=useState(false)
   const {t}=useTranslation()
   const handleClickOpenPopUp=()=>{
-  }
+    setOpen(!open)
+
+  } 
   return (
      
     <div className='reminder-cards-wrapper mt-22'>
         <div className='section-title'>
             <h5 className='d-flex align-items-center'>{t('PatientDashboard.Reminders.title')} <button type="button" onClick={handleClickOpenPopUp}><img src="/images/Add-Button-White.svg" alt="button" /></button></h5>
         </div>
+        {open &&
+          <WeightYourselfReminderOverlay />
+        }
         <div className='wrapper d-flex flex-wrap'>
             <ReminderCard/>
             <ReminderCard/>
