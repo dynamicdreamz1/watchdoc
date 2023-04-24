323f244a8cf74404872e08458757f4cdd1b4fa82
dynamic113dreamz <117342675+dynamic113dreamz@users.noreply.github.com>
2023-04-24 15:19:29 +0530

    add icon in reminder part

--- src/components/Patient/Reminder/Reminders.js
+++ src/components/Patient/Reminder/Reminders.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import ReminderCard from './ReminderCard'
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 
 export default function Reminders() {
   const {t}=useTranslation()
+  const handleClickOpenPopUp=()=>{
+  }
   return (
      
     <div className='reminder-cards-wrapper mt-22'>
         <div className='section-title'>
-            <h5>{t('PatientDashboard.Reminders.title')}</h5>
+            <h5>{t('PatientDashboard.Reminders.title')} </h5>
+            <div onClick={handleClickOpenPopUp}><AddCircleOutlineRoundedIcon /></div>
         </div>
         <div className='wrapper d-flex flex-wrap'>
             <ReminderCard/>
