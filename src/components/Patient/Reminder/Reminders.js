4a65c5543aaf29dbaf0d7594b39e9686991e482b
dynamic113dreamz <117342675+dynamic113dreamz@users.noreply.github.com>
2023-04-24 16:50:36 +0530

    add popup in reminder

--- src/components/Patient/Reminder/Reminders.js
+++ src/components/Patient/Reminder/Reminders.js
@@ -2,23 +2,35 @@ import React,{useState} from 'react'
 import { useTranslation } from 'react-i18next'
 import ReminderCard from './ReminderCard'
 import WeightYourselfReminderOverlay from '../../Clinician/Overlays/WeightYourselfReminderOverlay';
+import { Button, Dialog } from '@mui/material';
 
 export default function Reminders() {
   const [open,setOpen]=useState(false)
   const {t}=useTranslation()
-  const handleClickOpenPopUp=()=>{
-    setOpen(!open)
 
-  } 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
   return (
      
     <div className='reminder-cards-wrapper mt-22'>
         <div className='section-title'>
-            <h5 className='d-flex align-items-center'>{t('PatientDashboard.Reminders.title')} <button type="button" onClick={handleClickOpenPopUp}><img src="/images/Add-Button-White.svg" alt="button" /></button></h5>
+            <h5 className='d-flex align-items-center'>{t('PatientDashboard.Reminders.title')} <button type="button"  onClick={handleClickOpen}><img src="/images/Add-Button-White.svg" alt="button" /></button></h5>
         </div>
-        {open &&
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          className='reminder-overlay'
+        >
+          <button type='button' className='close-btn' onClick={handleClose}><img src='/images/Close-Icon.svg' alt='Close Button' /></button>
           <WeightYourselfReminderOverlay />
-        }
+        </Dialog>
         <div className='wrapper d-flex flex-wrap'>
             <ReminderCard/>
             <ReminderCard/>
