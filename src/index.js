40cc7cac1d6ca8c39e1fbeccbb3bae359452d3a5
dynamic113dreamz <117342675+dynamic113dreamz@users.noreply.github.com>
2023-05-12 18:57:23 +0530

    remove redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import './i18n/i18n'
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { Provider } from 'react-redux';
 
 
 
@@ -13,13 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <GoogleOAuthProvider clientId="555077241185-r79oaldvmmq001citu431g84i7jcup71.apps.googleusercontent.com">
-    <Provider >
     <BrowserRouter>
 
       <App />
 
     </BrowserRouter>
-    </Provider>
   </GoogleOAuthProvider>
 );
 
