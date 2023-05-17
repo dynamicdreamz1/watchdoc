270e7157ac1663d9de27d6ddd83f6507c02a53f8
dynamicdreamz1 <programmer113.dynamicdreamz@gmail.com>
2023-05-17 10:48:58 +0530

    status colour change

--- src/components/common/DetailCards/RiskStatusForBloodOxygen.js
+++ src/components/common/DetailCards/RiskStatusForBloodOxygen.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
+import MeasurementStatusForBloodOxygen from '../../Patient/Measurement/MeasurementStatusForBloodOxygen'
 
 export default function RiskStatusForBloodOxygen() {
   return (
     <>
     <div className='r-status d-flex align-items-center'>
-       {/* {el?.status && <span className='icon'></span>} */}
+    <MeasurementStatusForBloodOxygen  />       
+      
         <span className='text'>Good</span>
     </div>
     </>
--- src/components/common/DetailCards/RiskStatusForbloodPressure.js
+++ src/components/common/DetailCards/RiskStatusForbloodPressure.js
@@ -1,12 +1,11 @@
 
 import React from 'react'
-import MeasurementStatusForBloodOxygen from '../../Patient/Measurement/MeasurementStatusForBloodOxygen'
 
 export default function RiskStatusForbloodPressure() {
   return (
     <>
     <div className='r-status d-flex align-items-center'>
-    <MeasurementStatusForBloodOxygen  />       
+    <span className='normal m-status alert'></span>
     <span className='text'>Good</span>
     </div>
     
