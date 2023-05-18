01a7eed80747c3cc9d87c5cf3046b5b5a864d45f
dynamicdreamz1 <programmer113.dynamicdreamz@gmail.com>
2023-05-18 18:07:20 +0530

    week calender add

--- src/components/common/WeeklyDatePicker.js
+++ src/components/common/WeeklyDatePicker.js
@@ -1,8 +1,11 @@
-import * as React from 'react';
+import  React,{useState} from 'react';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { TextField } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 
 export default function WeekPickerComponent({setFinalDate}) {
   const [startDate, setStartDate] = React.useState(null);
@@ -25,6 +28,18 @@ export default function WeekPickerComponent({setFinalDate}) {
 
 
 
+  const [date, setDate] = useState({});
+  const [focus, setFocus] = useState(null);
+
+  const handleDatesChange = (dates) => {
+    setDate(dates);
+  };
+
+  const handleFocusChange = (focusedInput) => {
+    setFocus(focusedInput);
+  };
+ 
+
 
   // useEffect(()=>{
   //   if(setFinalDate !== undefined){
@@ -38,27 +53,40 @@ export default function WeekPickerComponent({setFinalDate}) {
 
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        label="Start Date"
-        value={startDate}
-        onChange={handleStartDateChange}
-        renderInput={(params) => <TextField {...params} />}
+    // <LocalizationProvider dateAdapter={AdapterDayjs}>
+    //   <DatePicker
+    //     label="Start Date"
+    //     value={startDate}
+    //     onChange={handleStartDateChange}
+    //     renderInput={(params) => <TextField {...params} />}
+    //   />
+    //   <DatePicker
+    //     label="End Date"
+    //     value={endDate}
+    //     onChange={handleEndDateChange}
+    //     renderInput={(params) => (
+    //       <TextField
+    //         {...params}
+    //         readOnly={!startDate} // Disable the input field when no start date is selected
+    //       />
+    //     )}
+    //     minDate={startDate} // Set minimum date as the selected start date
+    //     maxDate={dayjs(startDate).add(6, 'day').toDate()} // Set maximum date as 6 days ahead of the selected start date
+    //     disabled // Disable the end date field if no start date is selected
+    //   />
+    // </LocalizationProvider>
+
+
+    <div>
+      <DateRangePicker
+        startDate={date.startDate}
+        endDate={date.endDate}
+        focusedInput={focus}
+        onDatesChange={handleDatesChange}
+        onFocusChange={handleFocusChange}
+        startDateOffset={(day) => day.subtract(3, 'days')}
+        endDateOffset={(day) => day.add(3, 'days')}
       />
-      <DatePicker
-        label="End Date"
-        value={endDate}
-        onChange={handleEndDateChange}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            readOnly={!startDate} // Disable the input field when no start date is selected
-          />
-        )}
-        minDate={startDate} // Set minimum date as the selected start date
-        maxDate={dayjs(startDate).add(6, 'day').toDate()} // Set maximum date as 6 days ahead of the selected start date
-        disabled // Disable the end date field if no start date is selected
-      />
-    </LocalizationProvider>
+    </div>
   );
 }
