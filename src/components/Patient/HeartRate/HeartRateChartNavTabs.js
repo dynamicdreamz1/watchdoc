01a33749d06cdb5dc585a7bb88f17607d3e0d4ac
dynamicdreamz1 <programmer113.dynamicdreamz@gmail.com>
2023-05-19 13:08:14 +0530

    add default skeleton in heartrate

--- src/components/Patient/HeartRate/HeartRateChartNavTabs.js
+++ src/components/Patient/HeartRate/HeartRateChartNavTabs.js
@@ -2,14 +2,14 @@ import { Box, Tab, Tabs } from '@mui/material';
 import React from 'react'
 import ChartTitle from '../../common/Chart/ChartTitle';
 import { TabPanel } from '../../common/Tabs';
-import { a11yProps} from '../../../Utility/functions';
-import { ChartSkeleton } from '../../../Utility/Skeleton';
+import { a11yProps } from '../../../Utility/functions';
+import { ChartSkeleton, DefaultChartSkeleton } from '../../../Utility/Skeleton';
 import HeartRateChart from '../../common/Chart/HeartRateChart';
 
-export default function HeartRateChartNavTabs({action}) {
-    const {isHeartrateSkeleton,setTimeType,HeartRateAvg,setFinalDate,setHeartRateValue}=action;
+export default function HeartRateChartNavTabs({ action }) {
+    const { isHeartrateSkeleton, setTimeType, HeartRateAvg, setFinalDate, setHeartRateValue } = action;
     const [value, setValue] = React.useState(0);
-    const HeartData=HeartRateAvg?.data?.summary;
+    const HeartData = HeartRateAvg?.data?.summary;
 
     const handleChange = (event, newValue) => {
         const valueType = newValue === 0 ? 'daily' : newValue === 1 ? 'weekly' : newValue === 2 ? 'monthly' : "";
@@ -17,14 +17,15 @@ export default function HeartRateChartNavTabs({action}) {
         setTimeType(valueType)
         setValue(newValue);
     };
-       
-    const titleAction={
+
+    const titleAction = {
         setHeartRateValue,
         value,
         HeartData,
         setFinalDate,
-        dataKey:"heartrate"
+        dataKey: "heartrate"
     }
+
 
     return (
         <>
@@ -37,19 +38,25 @@ export default function HeartRateChartNavTabs({action}) {
                         <Tab label="Weekly" {...a11yProps(1)} />
                         <Tab label="Monthly" {...a11yProps(2)} />
                     </Tabs>
-                    
+
                     <ChartTitle setHeartRateValue titleAction={titleAction} />
                     {/* : <ChartResultRange />} */}
 
                 </Box>
                 <TabPanel value={value} index={0}>
-                {HeartRateAvg?.data?.details?.length===0 || isHeartrateSkeleton? <ChartSkeleton />:<HeartRateChart  HeartData={HeartRateAvg} />}
+                    {HeartRateAvg?.data?.details?.length === 0 ? <DefaultChartSkeleton /> :
+                        isHeartrateSkeleton ? <ChartSkeleton /> :
+                            <HeartRateChart HeartData={HeartRateAvg} />}
                 </TabPanel>
                 <TabPanel value={value} index={1}>
-                   {HeartRateAvg?.data?.details?.length===0 || isHeartrateSkeleton ? <ChartSkeleton />: <HeartRateChart  HeartData={HeartRateAvg} />}
+                    {HeartRateAvg?.data?.details?.length === 0 ? <DefaultChartSkeleton /> :
+                        isHeartrateSkeleton ? <ChartSkeleton /> :
+                            <HeartRateChart HeartData={HeartRateAvg} />}
                 </TabPanel>
                 <TabPanel value={value} index={2}>
-                   {HeartRateAvg?.data?.details?.length===0 || isHeartrateSkeleton ? <ChartSkeleton />: <HeartRateChart  HeartData={HeartRateAvg} />}
+                    {HeartRateAvg?.data?.details?.length === 0 ? <DefaultChartSkeleton /> :
+                        isHeartrateSkeleton ? <ChartSkeleton /> :
+                            <HeartRateChart HeartData={HeartRateAvg} />}
                 </TabPanel>
 
             </Box>
