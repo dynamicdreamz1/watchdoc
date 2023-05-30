86cf71aa8513bd1704579bc073923e602bb60fb9
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2023-05-30 16:44:29 +0530

    fixed  issue

--- src/components/Patient/Reminder/RemindDay.js
+++ src/components/Patient/Reminder/RemindDay.js
@@ -1,12 +1,7 @@
 import React from 'react'
 
 export default function RemindDay({reminderDay}) {
-  // const [open, setOpen] = React.useState(false);
-
-  // const handleClickOpen = () => {
-  //   setOpen(true);
-  // };
-
+  
   const mon = reminderDay.includes("1");
   const tue = reminderDay.includes("2");
   const wen = reminderDay.includes("3");
@@ -16,25 +11,11 @@ export default function RemindDay({reminderDay}) {
   const sun = reminderDay.includes("7");
 
 
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
-
   return (
     <>
     <div className='days'>
         <button type="button" className={mon=== true ? "day active" : "day"}>M</button>
-        {/* <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-          className='reminder-overlay'
-        >
-          <button type='button' className='close-btn' onClick={handleClose}><img src='/images/Close-Icon.svg' alt='Close Button' /></button>
-          <HighHeartrateOverlay/>
-        </Dialog> */}
-        <button type="button" className={tue=== true ? "day active" : "day"}>T</button>
+            <button type="button" className={tue=== true ? "day active" : "day"}>T</button>
             <button type="button" className={wen=== true ? "day active" : "day"}>W</button>
             <button type="button" className={thu=== true ? "day active" : "day"}>T</button>
             <button type="button" className={fri=== true ? "day active" : "day"}>F</button>
