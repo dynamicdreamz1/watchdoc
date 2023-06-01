77d8ca35cc2da2dd3e13d25a1a60b221102ae3f5
dynamicdreamz1 <programmer113.dynamicdreamz@gmail.com>
2023-06-01 18:31:12 +0530

    view all and view less in clitical alerts

--- src/components/common/Alerts/CriticalAlerts.js
+++ src/components/common/Alerts/CriticalAlerts.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AlertCard from './AlertCard'
 
 export default function CriticalAlerts({latestData}) {
+  const [viewAllBtn,setViewAllBtn]=useState(false)
+  
+  const viewLessData=latestData?.criteria_alert && latestData?.criteria_alert?.slice(0,4);
+  const viewAllData=latestData?.criteria_alert && latestData?.criteria_alert?.slice(0,latestData?.criteria_alert?.length)
+  const finalData=viewAllBtn?viewAllData:viewLessData
   return (
     <>
         <div className='critical-alerts-wrapper mt-22'>
             <div className="section-title d-flex align-items-center justify-content-between">
                 <h5>Critical Alerts</h5>
-                <button type='button'>View All Alerts (41)</button>
+                <button type='button' onClick={()=>setViewAllBtn(!viewAllBtn)}>{!viewAllBtn?`View All Alerts (${finalData?.length?finalData?.length:0})`:`View Less Alerts (${finalData?.length?finalData?.length:0})`}</button>
             </div>
             <div className='wrapper'>
               {
-                latestData?.criteria_alert?.map((item, I) => {
+               finalData?.map((item, I) => {
                 return <> <AlertCard alertData={item} key={I}/> <br/></>
               })
               }
