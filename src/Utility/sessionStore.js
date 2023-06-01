ba8aa0abfcae4df4a17d047f2bb0e1c08906d739
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2023-06-01 14:35:21 +0530

    fixed issue

--- src/Utility/sessionStore.js
+++ src/Utility/sessionStore.js
@@ -1,6 +1,5 @@
 import { EncryptStorage } from 'encrypt-storage';
 
-
 export const StoreCookie = new EncryptStorage('DynamicKeyHAS', {
     prefix: '@instance1',
     storageType: 'sessionStorage',
