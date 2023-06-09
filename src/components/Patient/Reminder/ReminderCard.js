ef5d96d3721d40eae528b7f734a39300687854a1
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2023-06-09 14:32:17 +0530

    fixed issue

--- src/components/Patient/Reminder/ReminderCard.js
+++ src/components/Patient/Reminder/ReminderCard.js
@@ -3,6 +3,7 @@ import RemindDay from './RemindDay'
 import ReminderIcon from './ReminderIcon'
 import ReminderOptions from './ReminderOptions'
 import ReminderTime from './ReminderTime'
+import ReminderInput from './ReminderInput'
 import ReminderTitle from './ReminderTitle'
 import { DeletereminderCard } from '../../../services/ClinicianService'
 import Swal from 'sweetalert2'
@@ -11,8 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 export default function ReminderCard({reminderData,latestData,fetchData}) {
-  
-  const handleClickDeleteReminder=async()=>{
+    const handleClickDeleteReminder=async()=>{
     const formData=new FormData()
     formData.append("user_id",latestData?.user_data?.id)
     formData.append("reminder_id",reminderData?.reminder_id)
@@ -68,6 +68,7 @@ export default function ReminderCard({reminderData,latestData,fetchData}) {
             <ReminderTitle reminderType={reminderData?.reminder_type}/>
             <RemindDay reminderDay={reminderData?.day}/>
             <ReminderTime reminderTime={reminderData?.time}/>
+            <ReminderInput reminderInput={reminderData?.reminder_title}/>
         </div>
     </div>
     </>
