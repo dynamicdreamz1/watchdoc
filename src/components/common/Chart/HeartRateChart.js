ebc7566a86741bb7c9afbd8a5cad0a5dd2622c3c
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2023-06-09 10:09:47 +0530

    fixed issuee

--- src/components/common/Chart/HeartRateChart.js
+++ src/components/common/Chart/HeartRateChart.js
@@ -68,11 +68,8 @@ export default function HeartRateChart({HeartData,value}) {
         }
 		return (
 		<>
-    
 			<CanvasJSChart options = {options}
-				/* onRef={ref => this.chart = ref} */
 			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
 		</>
 		);
 }
