9a27affb1d06ac454e179b4b54f8a31e6b97621a
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2023-06-13 15:38:09 +0530

    fixed issue

--- src/components/Patient/LinkDevices/LinkDevices.js
+++ src/components/Patient/LinkDevices/LinkDevices.js
@@ -1,80 +1,125 @@
-import React from 'react'
+import React from "react";
+import { connectDevice } from "../../../services/PatientsService";
 
 export default function LinkDevices() {
+
+  const onConnect = (e) => {
+    e.preventDefault();
+
+    connectDevice()
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  };
+
   return (
     <>
-      <div className='devices-wrapper'>
-        <div className='device-block'>
-          <div className='text-block d-flex align-items-center'>
-            <span className='icon d-flex justify-content-center'><img src='/images/Fitbit-icon.svg' alt='Fitbit Icon' /></span>
-            <span className='text'>Fitbit</span>
+      <div className="devices-wrapper">
+        <div className="device-block">
+          <div className="text-block d-flex align-items-center">
+            <span className="icon d-flex justify-content-center">
+              <img src="/images/Fitbit-icon.svg" alt="Fitbit Icon" />
+            </span>
+            <span className="text">Fitbit</span>
           </div>
-          <div className='btn-block'>
-            <button type='button' className='btn'>Connect</button>
-          </div>
-        </div>
-
-        <div className='device-block'>
-          <div className='text-block d-flex align-items-center'>
-            <span className='icon d-flex justify-content-center'><img src='/images/Apple-Health-icon.svg' alt="Apple Health Icon" /></span>
-            <span className='text'>Apple Health</span>
-          </div>
-          <div className='btn-block'>
-            <button type='button' className='btn'>Connect</button>
+          <div className="btn-block">
+            <button onClick={(e) => onConnect(e)} type="button" className="btn">
+              Connect
+            </button>
           </div>
         </div>
 
-        <div className='device-block'>
-          <div className='text-block d-flex align-items-center'>
-            <span className='icon d-flex justify-content-center'><img src='/images/Garmin-icon.svg' alt="Garmin Icon" /></span>
-            <span className='text'>Garmin</span>
+        <div className="device-block">
+          <div className="text-block d-flex align-items-center">
+            <span className="icon d-flex justify-content-center">
+              <img
+                src="/images/Apple-Health-icon.svg"
+                alt="Apple Health Icon"
+              />
+            </span>
+            <span className="text">Apple Health</span>
           </div>
-          <div className='btn-block'>
-            <button type='button' className='btn'>Connect</button>
-          </div>
-        </div>
-
-        <div className='device-block'>
-          <div className='text-block d-flex align-items-center'>
-            <span className='icon d-flex justify-content-center'><img src='/images/Google-FIt-icon.svg' alt="Google Fit Icon" /></span>
-            <span className='text'>Google FIt</span>
-          </div>
-          <div className='btn-block'>
-            <button type='button' className='btn'>Connect</button>
+          <div className="btn-block">
+            <button type="button" className="btn">
+              Connect
+            </button>
           </div>
         </div>
 
-        <div className='device-block'>
-          <div className='text-block d-flex align-items-center'>
-            <span className='icon d-flex justify-content-center'><img src='/images/Oura-icon.svg' alt="Oura Icon"/></span>
-            <span className='text'>Oura</span>
+        <div className="device-block">
+          <div className="text-block d-flex align-items-center">
+            <span className="icon d-flex justify-content-center">
+              <img src="/images/Garmin-icon.svg" alt="Garmin Icon" />
+            </span>
+            <span className="text">Garmin</span>
           </div>
-          <div className='btn-block'>
-            <button type='button' className='btn'>Connect</button>
-          </div>
-        </div>
-
-        <div className='device-block'>
-          <div className='text-block d-flex align-items-center'>
-            <span className='icon d-flex justify-content-center'><img src='/images/Samsung-icon.svg' alt="Samsung Icon" /></span>
-            <span className='text'>Samsung</span>
-          </div>
-          <div className='btn-block'>
-            <button type='button' className='btn'>Connect</button>
+          <div className="btn-block">
+            <button type="button" className="btn">
+              Connect
+            </button>
           </div>
         </div>
 
-        <div className='device-block'>
-          <div className='text-block d-flex align-items-center'>
-            <span className='icon d-flex justify-content-center'><img src='/images/Withings-icon.svg' alt="Withings Icon" /></span>
-            <span className='text'>Withings</span>
+        <div className="device-block">
+          <div className="text-block d-flex align-items-center">
+            <span className="icon d-flex justify-content-center">
+              <img src="/images/Google-FIt-icon.svg" alt="Google Fit Icon" />
+            </span>
+            <span className="text">Google FIt</span>
           </div>
-          <div className='btn-block'>
-            <button type='button' className='btn'>Connect</button>
+          <div className="btn-block">
+            <button type="button" className="btn">
+              Connect
+            </button>
+          </div>
+        </div>
+
+        <div className="device-block">
+          <div className="text-block d-flex align-items-center">
+            <span className="icon d-flex justify-content-center">
+              <img src="/images/Oura-icon.svg" alt="Oura Icon" />
+            </span>
+            <span className="text">Oura</span>
+          </div>
+          <div className="btn-block">
+            <button type="button" className="btn">
+              Connect
+            </button>
+          </div>
+        </div>
+
+        <div className="device-block">
+          <div className="text-block d-flex align-items-center">
+            <span className="icon d-flex justify-content-center">
+              <img src="/images/Samsung-icon.svg" alt="Samsung Icon" />
+            </span>
+            <span className="text">Samsung</span>
+          </div>
+          <div className="btn-block">
+            <button type="button" className="btn">
+              Connect
+            </button>
+          </div>
+        </div>
+
+        <div className="device-block">
+          <div className="text-block d-flex align-items-center">
+            <span className="icon d-flex justify-content-center">
+              <img src="/images/Withings-icon.svg" alt="Withings Icon" />
+            </span>
+            <span className="text">Withings</span>
+          </div>
+          <div className="btn-block">
+            <button type="button" className="btn">
+              Connect
+            </button>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
-
--- src/services/PatientsService.js
+++ src/services/PatientsService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { headersClinician } from "../Utility/functions";
+// import fetch from 'node-fetch';
+
 
 export async function getLatestMeasurement() {
     try {
@@ -60,3 +62,29 @@ export const AddEmergencyContact=async(data)=>{
     }
 
 }
+
+export const connectDevice = async () => {
+    // const options = {
+    //     method: 'POST',
+    //     headers: {
+    //       accept: 'application/json',
+    //       'dev-id': 'testingTerra',
+    //       'content-type': 'application/json',
+    //       'x-api-key': 'ussv5SAQ53a1nNTxsMr9G41zj2KUhYMk5eDU1hjG'
+    //     },
+    //     body: JSON.stringify({
+    //       providers: 'GARMIN,WITHINGS,FITBIT,GOOGLE,OURA,WAHOO,PELOTON,ZWIFT,TRAININGPEAKS,FREESTYLELIBRE,DEXCOM,COROS,HUAWEI,OMRON,RENPHO,POLAR,SUUNTO,EIGHT,APPLE,CONCEPT2,WHOOP,IFIT,TEMPO,CRONOMETER,FATSECRET,NUTRACHECK,UNDERARMOUR',
+    //       language: 'en'
+    //     })
+    //   };
+      
+    //   fetch('https://api.tryterra.co/v2/auth/generateWidgetSession', options)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       console.log(data);
+    //       console.log("response", data);
+    //     })
+    //     .catch(error => {
+    //       console.error(error);
+    //     });
+  };
