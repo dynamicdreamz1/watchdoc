4b1eee43e86a444fae1de4bb285b9cee36fde955
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2023-06-15 10:41:10 +0530

    fixed issue

--- src/components/Clinician/CliniciansTableTabs.js
+++ src/components/Clinician/CliniciansTableTabs.js
@@ -146,7 +146,7 @@ let [options, setOptions] = useState(specificOption)
     setDataLimit(pageCount)
   };
 
-  const pageOptions=[10,20,30,40,50,60,70,80,90,100];
+  const pageOptions=[5,10,20,30,40,50,60,70,80,90,100];
 
   return (
     <>
