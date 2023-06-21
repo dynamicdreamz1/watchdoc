a114b255140bd3e9ddae21f061fea191961813b4
dynamicdreamz1 <programmer113.dynamicdreamz@gmail.com>
2023-06-21 17:58:04 +0530

    changes in search bar place holder text

--- src/components/common/SearchBar.js
+++ src/components/common/SearchBar.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
+import { getCurrentUserData } from '../../services/UserService';
 
 
 export default function SearchBar({setSearch}) {
-  const { t } = useTranslation();
+  const userData=getCurrentUserData();
 
   const searchInpitData = (e) =>{
     if (setSearch) {
       setSearch(e?.target?.value)
     }
   }
+const placeHolderText=
+userData?.roles[0]?.name==='User'?"Search For Clinician":userData?.roles[0]?.name==='Clinician'?"Search For Patients":
+userData?.roles[0]?.name==='Admin'?"Search For clinician and patient":""
 
   return (
     <>
       <form method='post' className='search-block'>
-        <input onChange={(e)=>searchInpitData(e)} type="search" name="search" placeholder={t('DashboardPage.d4')} />
+        <input onChange={(e)=>searchInpitData(e)} type="search" name="search" placeholder={placeHolderText} />
       </form>
     </>
   )
