a9142ac049e7f8753ef9f4a564a42850a832f39f
dynamicdreamz1 <programmer113.dynamicdreamz@gmail.com>
2023-06-21 18:07:11 +0530

    initial chages

--- src/components/common/SearchBar.js
+++ src/components/common/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { getCurrentUserData } from '../../services/UserService';
+import { useLocation } from 'react-router-dom';
 
 
 export default function SearchBar({setSearch}) {
   const userData=getCurrentUserData();
+  const location = useLocation()
 
   const searchInpitData = (e) =>{
     if (setSearch) {
@@ -11,8 +13,8 @@ export default function SearchBar({setSearch}) {
     }
   }
 const placeHolderText=
-userData?.roles[0]?.name==='User'?"Search For Clinician":userData?.roles[0]?.name==='Clinician'?"Search For Patients":
-userData?.roles[0]?.name==='Admin'?"Search For clinician and patient":""
+userData?.roles[0]?.name==='User' || (userData?.roles[0]?.name==='Admin'&& location?.pathname==='/clinicians')?"Search For Clinician":userData?.roles[0]?.name==='Clinician' || 
+(userData?.roles[0]?.name==='Admin'&& location?.pathname==='/adminpatient')?"Search For Patients":""
 
   return (
     <>
