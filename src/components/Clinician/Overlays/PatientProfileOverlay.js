e77c03bc63012b27ffffcaa8936a6a3b60f17e4e
dynamicdreamz1 <programmer113.dynamicdreamz@gmail.com>
2023-06-26 11:07:17 +0530

    initial changes

--- src/components/Clinician/Overlays/PatientProfileOverlay.js
+++ src/components/Clinician/Overlays/PatientProfileOverlay.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Formik } from "formik";
+import { Field, Formik } from "formik";
 import { MetaFormeting } from "../../../Utility/functions";
 
 export const PatientProfileOverlay = ({ handleClose, data }) => {
   const { first_name, last_name, sex, dob, height, weight } = MetaFormeting(data?.user_data);
   const { t } = useTranslation();
+
+  const [patientData,setPatientData]=useState({
+    "first_name": first_name || "",
+    "last_name": last_name || "",
+    "email": data?.user_data?.email || "",
+    "dob": dob || "",
+    "sex": sex || "",
+    "weight": weight || "",
+    "height": height || "",
+    "contact_number": data?.user_data?.contact_number || ""
+  })
+
+
+
 
   const handleSubmitForm = (data) => {
     handleClose();
@@ -13,7 +27,7 @@ export const PatientProfileOverlay = ({ handleClose, data }) => {
 
   return (
     <Formik
-      initialValues={""}
+      initialValues={patientData}
       enableReinitialize={true}
       validationSchema=""
       onSubmit={(values) => {
@@ -25,14 +39,15 @@ export const PatientProfileOverlay = ({ handleClose, data }) => {
           <div className="dialog-title">
             <h2>Profile</h2>
           </div>
-          <form id="main_form">
+          <form id="main_form" onSubmit={props.handleSubmit}>
             <div className="input-block">
               <label htmlFor="exampleInputFirstName">First name</label>
               <input
                 type="text"
-                name="firstName"
+                name="first_name"
                 placeholder={t("EditProfilePage.form.f13")}
-                value={first_name}
+                value={props?.values?.first_name}
+                onChange={props?.handleChange}
                 id="exampleInputFirstName"
               />
             </div>
@@ -40,17 +55,18 @@ export const PatientProfileOverlay = ({ handleClose, data }) => {
               <label htmlFor="exampleInputLastName">Last name</label>
               <input
                 type="text"
-                name="lastName"
+                name="last_name"
                 placeholder={t("EditProfilePage.form.f14")}
-                value={last_name}
+                value={props?.values?.last_name}
+                onChange={props?.handleChange}
                 id="exampleInputLastName"
               />
             </div>
             <div className="input-block">
-              <label htmlFor="exampleInputDOB">Date of birth</label>
-              <input type="text" name="dob" value={dob} id="exampleInputDOB" />
+              <label htmlFor="exampleInputDOB">Date of birth</label>                
+              <input type="text" name="dob" value={props?.values?.dob} onChange={props?.handleChange}  id="exampleInputDOB" />
             </div>
-            <div className="input-block">
+            {/* <div className="input-block">
               <label htmlFor="exampleInputSex">
                 {t("EditProfilePage.form.f4")}
               </label>
@@ -88,35 +104,55 @@ export const PatientProfileOverlay = ({ handleClose, data }) => {
                   <label htmlFor="other">{t("EditProfilePage.form.f12")}</label>
                 </div>
               </div>
-            </div>
+            </div> */}
+            <div className='input-block'>
+                                <label htmlFor="exampleInputSex" >{t('CreateProfilePage.form.f4')}</label>
+                                <div className='radio-buttons'>
+                                    <div className='radio-button'>
+                                        <Field type="radio" id="male" name="sex" value="male" />
+                                        <label htmlFor="male">Male</label>
+                                    </div>
+                                    <div className='radio-button'>
+                                        <Field type="radio" id="female" name="sex" value="female" />
+                                        <label htmlFor="female">Female</label>
+                                    </div>
+                                    <div className='radio-button'>
+                                        <Field type="radio" id="other" name="sex" value="other" />
+                                        <label htmlFor="other">Other</label>
+                                    </div>
+                                </div>
+                            </div>
             <div className="input-block">
               <label htmlFor="exampleInputHeight">Height (cm)</label>
               <input
                 type="text"
                 name="height"
                 placeholder={t("EditProfilePage.form.f16")}
-                value={height}
+                value={props?.values?.height}
+                onChange={props?.handleChange}
                 id="exampleInputHeight"
               />
             </div>
             <div className="input-block">
               <label>Email address</label>
-              <input type="email" name="email" value={data?.user_data?.email} />
+              <input type="email" name="email" value={props?.values?.email} onChange={props?.handleChange}/>
             </div>
             <div className="input-block">
               <label>Weight</label>
-              <input type="email" name="email" value={weight} />
+              <input type="text" name="weight" value={props?.values?.weight} onChange={props?.handleChange}/>
             </div>
             <div className="input-block country-code">
               <label id="country-code">Contact number</label>
               <div className="inputs-wrapper">
                 <input
                   type="text"
-                  name="number"
-                  value={data?.user_data?.contact_number}
+                  name="contact_number"
+                  value={props?.values?.contact_number}
+                  onChange={props?.handleChange}
                 ></input>
               </div>
             </div>
+            <button type="submit">{t('EditProfilePage.form.f9')}</button>
           </form>
         </>
       )}
