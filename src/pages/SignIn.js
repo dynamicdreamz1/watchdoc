3332c8895ab2dad6bcfa717e423ccf626cfe79be
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2024-05-06 11:53:08 +0530

    fixed isue when getting setserch isue

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,7 @@
 
-import React, { useContext,useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link, redirect, useNavigate } from 'react-router-dom';
-import {UserLogin } from '../services/UserService';
+import { UserLogin } from '../services/UserService';
 import '../css/Register.css'
 import { useTranslation } from 'react-i18next';
 import { Base64 } from 'js-base64';
@@ -15,26 +15,26 @@ const SignIn = () => {
 
     const user = useContext(UserContext);
     let navigate = useNavigate()
-    const [loginData]=useState({
-        "email":"",
-        "password":""
+    const [loginData] = useState({
+        "email": "",
+        "password": ""
     })
     const [error, setError] = useState('')
-    const [showPassword, setShowPassword] =useState(false)       
+    const [showPassword, setShowPassword] = useState(false)
     const [loading, setLoading] = useState(false)
     const { t } = useTranslation()
 
-    const LoginSchema = Yup.object({        
+    const LoginSchema = Yup.object({
         email: Yup.string().required(t('SignUpPage.validation.email.v1'))
-         // eslint-disable-next-line no-useless-escape
+            // eslint-disable-next-line no-useless-escape
             .matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, t('SignUpPage.validation.email.v2')),
-        password:Yup.string()
+        password: Yup.string()
             .required("Password is required*")
             .matches(
                 // eslint-disable-next-line no-useless-escape
                 /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
                 "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-            ),    
+            ),
     });
 
 
@@ -46,7 +46,7 @@ const SignIn = () => {
         setLoading(true)
         UserLogin(data)
             .then((response) => {
-                if(response?.status===200){
+                if (response?.status === 200) {
                     let encodedemail = Base64.encode(response?.data?.email)
                     // let encodedemail = Base64.encode(data?.email)
 
@@ -54,17 +54,17 @@ const SignIn = () => {
                     setLoading(false)
                     navigate(`/twofactoreverification/${encodedemail}`, {
                         state: {
-                          id: response?.data?.verification_code,
-                          emailId: encodedemail,
+                            id: response?.data?.verification_code,
+                            emailId: encodedemail,
                         },
-                      });
+                    });
                     // navigate(`/twofactoreverification/${encodedemail}/${response?.data?.verification_code}`)
 
                 }
                 else {
                     setError(response)
                     setLoading(false)
-                    setTimeout(() => {setError("")},2000);
+                    setTimeout(() => { setError("") }, 2000);
 
                 }
                 //  else {
@@ -84,7 +84,7 @@ const SignIn = () => {
                 // }
             })
             .catch((error) => {
-               setError(error)
+                setError(error)
                 return error
             })
 
@@ -95,69 +95,69 @@ const SignIn = () => {
     }
     return (
         <Formik
-        initialValues={loginData}
-        enableReinitialize={true}
-        validationSchema={LoginSchema}
-        onSubmit={(values) => { handleSubmitForm(values) }}
-    >
-        {(props) => (
-        <React.Fragment>
-            <div className='page-wrapper'>
-                <div className='signin-box'>
-                    <div className='logo-block'>
-                        <div className='logo'>
-                            <img src='/images/WatchDoc-LOGO.svg' alt="WatchDoc Logo" />
+            initialValues={loginData}
+            enableReinitialize={true}
+            validationSchema={LoginSchema}
+            onSubmit={(values) => { handleSubmitForm(values) }}
+        >
+            {(props) => (
+                <React.Fragment>
+                    <div className='page-wrapper'>
+                        <div className='signin-box'>
+                            <div className='logo-block'>
+                                <div className='logo'>
+                                    <img src='/images/WatchDoc-LOGO.svg' alt="WatchDoc Logo" />
+                                </div>
+                            </div>
+                            <div className='form-block'>
+                                <div className='form-title text-center'>
+                                    <h1>{t('SignInPage.heading')}</h1>
+                                </div>
+
+                                <form onSubmit={props.handleSubmit} autoComplete="off">
+                                    <div className='input-block'>
+                                        <input type="email" placeholder={t('SignInPage.form.f1')} name="email"
+                                            value={props?.values?.email} id="exampleInputEmail1" aria-describedby="emailHelp" onChange={props.handleChange} />
+                                        <span className="error">{props?.errors?.email ? props?.errors?.email : ""}</span>
+
+                                    </div>
+                                    <div className='input-block'>
+                                        <input name="password" type={showPassword ? "text" : "password"} onChange={props.handleChange}
+                                            value={props?.values?.password} placeholder={t('SignInPage.form.f7')} />
+                                        <button className='show-hide' type="button" onClick={() => setShowPassword(!showPassword)}>
+                                            {showPassword ?
+                                                <RemoveRedEyeOutlinedIcon /> :
+                                                <VisibilityOffOutlinedIcon />
+                                            }
+                                        </button>
+                                        <span className="error">{props?.errors?.password ? props?.errors?.password : ""}</span>
+
+
+                                    </div>
+                                    <div className='forgot-pw-block'>
+                                        <Link to='/forgetpassword'>{t('SignInPage.form.f11')}</Link>
+                                    </div>
+                                    <div className='LoginError'>{error && error}</div>
+
+                                    <div className='submit-block'>
+                                        <button type="submit">{t('SignInPage.form.f2')}</button>
+                                    </div>
+                                    {loading ? <b>{t('SignInPage.loader.l1')}</b> : ""}
+                                    <div className='not-user-block text-center'>
+                                        <span>{t('SignInPage.form.f8')}</span>
+                                        <Link to='/signup'>{t('SignInPage.form.f9')}</Link>
+                                    </div>
+                                    <div className='privacy-policy-text text-center'>
+                                        <a href="#">{t('SignInPage.form.f10')}</a>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                    <div className='form-block'>
-                        <div className='form-title text-center'>
-                            <h1>{t('SignInPage.heading')}</h1>
-                        </div>
 
-                        <form onSubmit={props.handleSubmit} autoComplete="off">
-                            <div className='input-block'>
-                                <input type="email" placeholder={t('SignInPage.form.f1')}  name="email"
-                                value={props?.values?.email} id="exampleInputEmail1" aria-describedby="emailHelp" onChange={props.handleChange}/>
-                                    <span className="error">{props?.errors?.email ? props?.errors?.email : ""}</span>
-
-                            </div>
-                            <div className='input-block'>
-                                <input name="password" type={showPassword ? "text" : "password"} onChange={props.handleChange}
-                                 value={props?.values?.password} placeholder={t('SignInPage.form.f7')} />
-                                <button className='show-hide' type="button" onClick={()=>setShowPassword(!showPassword)}>
-                                    {showPassword?
-                                    <RemoveRedEyeOutlinedIcon/>:
-                                    <VisibilityOffOutlinedIcon/>
-}
-                                </button>
-                                <span className="error">{props?.errors?.password ? props?.errors?.password : ""}</span>
-
-
-                            </div>
-                            <div className='forgot-pw-block'>
-                                <Link to='/forgetpassword'>{t('SignInPage.form.f11')}</Link>
-                            </div>
-                        <div className='LoginError'>{error && error}</div>
-
-                            <div className='submit-block'>
-                                <button type="submit">{t('SignInPage.form.f2')}</button>
-                            </div>
-                            {loading ? <b>{t('SignInPage.loader.l1')}</b> : ""}
-                            <div className='not-user-block text-center'>
-                                <span>{t('SignInPage.form.f8')}</span>
-                                <Link to='/signup'>{t('SignInPage.form.f9')}</Link>
-                            </div>
-                            <div className='privacy-policy-text text-center'>
-                                <a href={{}}>{t('SignInPage.form.f10')}</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-        </React.Fragment>
-         )}
-         </Formik>
+                </React.Fragment>
+            )}
+        </Formik>
     )
 }
 
