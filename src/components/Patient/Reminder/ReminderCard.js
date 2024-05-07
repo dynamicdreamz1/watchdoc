a8fe3cdac1e534108d18a274517cb7554b4b630b
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2024-05-07 15:35:06 +0530

    resolve reminder issue

--- src/components/Patient/Reminder/ReminderCard.js
+++ src/components/Patient/Reminder/ReminderCard.js
@@ -7,29 +7,29 @@ import ReminderInput from './ReminderInput'
 import ReminderTitle from './ReminderTitle'
 import { DeletereminderCard } from '../../../services/ClinicianService'
 import Swal from 'sweetalert2'
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-export default function ReminderCard({reminderData,latestData,fetchData}) {
-    const handleClickDeleteReminder=async()=>{
-    const formData=new FormData()
-    formData.append("user_id",latestData?.user_data?.id)
-    formData.append("reminder_id",reminderData?.reminder_id)
+export default function ReminderCard({ reminderData, latestData, fetchData }) {
+  const handleClickDeleteReminder = async () => {
+    const formData = new FormData()
+    formData.append("user_id", latestData?.user_data?.id)
+    formData.append("reminder_id", reminderData?.reminder_id)
     try {
       const result = await Swal.fire({
         title: 'Are you sure?',
         text: 'You want to delete reminder',
-        limit:1,
+        limit: 1,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#d33',
         cancelButtonColor: '#3085d6',
         confirmButtonText: 'Yes, delete it!',
       });
-  
+
       if (result.isConfirmed) {
-        const res = await DeletereminderCard(formData);       
+        const res = await DeletereminderCard(formData);
         await fetchData()
         toast.success(res?.data?.message, {
           position: 'top-right',
@@ -54,23 +54,21 @@ export default function ReminderCard({reminderData,latestData,fetchData}) {
 
       });
     }
-    
-
-}
+  }
   return (
     <>
-    <div className='reminder-card'>
-        <ReminderOptions handleClickDeleteReminder={handleClickDeleteReminder}/>
+      <div className='reminder-card'>
+        <ReminderOptions handleClickDeleteReminder={handleClickDeleteReminder} />
         <div className='icon-block'>
-            <ReminderIcon/>
+          <ReminderIcon />
         </div>
         <div className='content-block'>
-            <ReminderTitle reminderType={reminderData?.reminder_type}/>
-            <RemindDay reminderDay={reminderData?.day}/>
-            <ReminderTime reminderTime={reminderData?.time}/>
-            <ReminderInput reminderInput={reminderData?.reminder_title}/>
+          <ReminderTitle reminderType={reminderData?.reminder_type} />
+          <RemindDay reminderDay={reminderData?.day} />
+          <ReminderTime reminderTime={reminderData?.time} />
+          <ReminderInput reminderInput={reminderData?.reminder_title} />
         </div>
-    </div>
+      </div>
     </>
   )
 }
