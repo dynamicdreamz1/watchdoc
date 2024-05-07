302da91948ea9bc2b1c255c498b5172a0eb55a64
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2024-05-07 18:47:28 +0530

    fixed issue

--- src/components/common/Table/Status.js
+++ src/components/common/Table/Status.js
@@ -14,7 +14,7 @@ export default function Status(props) {
   const handleStatusChange = (e) => {
     handleClickStatus(el.id, e.target.value);
   };
-  return (
+    return (
     <>
       {(value === 2 && userData?.roles[0]?.name !== 'Admin') ?
         <select value={selectedStatus} onChange={handleStatusChange} className={props?.el?.status === "Reviewed" ? "text color-light-green" : "text color-light-blue"}>
@@ -35,8 +35,10 @@ export default function Status(props) {
           :
           <div className='wt table-data'>
             <Button variant="outlined" color="secondary" className={props?.el?.status === "Reviewed" ? "text color-light-green" : "text color-light-blue"}> <span className={props?.el?.status === "Reviewed" ? "text color-light-green" : "text color-light-blue"} onClick={() => {
-              if (value === 1) {
+              if (value === 0) {
+                handleClickStatus(el.alertId)
               } else if (value === 2) {
+
               } else {
                 handleClickStatus(el?.id, el?.status);
               }
